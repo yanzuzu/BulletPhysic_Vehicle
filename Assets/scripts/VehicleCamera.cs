ff8893fa2f13d13a844b6ca7255f1952e6231668
Sam Lo <sam@randomidea.com>
2016-08-21 14:08:12 +0800

    add camera to follow car.

--- Assets/scripts/VehicleCamera.cs
+++ Assets/scripts/VehicleCamera.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 
 public class VehicleCamera : MonoBehaviour {
+	public Transform target;
+	public float dist = 5f;
+	public float height = 30f;
+	private Transform m_cameraTrans;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+		m_cameraTrans = this.gameObject.transform;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate () {
+		if (null == target)
+			return;
+		m_cameraTrans.position = target.position;
+		m_cameraTrans.position -= target.forward * dist;
+		m_cameraTrans.position = new Vector3 (m_cameraTrans.position.x, height, m_cameraTrans.position.z);
+
+		m_cameraTrans.LookAt (target);
 	}
 }
